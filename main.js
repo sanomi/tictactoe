deaf9c0197f6ada51032322241498e96b14b225f
Austin Ezell <austin.r.ezell@gmail.com>
2015-09-11 15:13:26 -0700

    minor changes and bugfixes

--- main.js
+++ main.js
@@ -4,45 +4,40 @@ $(document).ready(function() {
 	var xFilled = [];
 	var oFilled = [];
 	var $title = $('#title');
-	console.log($title.text())
-	if ($title.text() === 'Tic Tac Toe!') {
-		$('.box').on('click', function() {
-			var $this = $(this);
-			if( $this.text() !== 'X' && $this.text() !== 'O' ) {
-				console.log($this.text());
-				if (count%2 === 0) {
-					$this.empty().append("<div class='text'>X</div>");
-					xFilled.push(parseInt($this.attr('id')));
-					xFilled = xFilled.sort();
-					console.log(xFilled);
-				} else {
-					$this.empty().append("<div class='text'>O</div>");
-					oFilled.push(parseInt($this.attr('id')));
-					oFilled= oFilled.sort();
-					console.log(oFilled);
-				} 
-			}
-		count++;
-		var lastX = xFilled[xFilled.length-1];
-		var lastO = oFilled[oFilled.length-1];
-		if ( 
-			( (xFilled[0] === 1 || xFilled[0] === 4 || xFilled[0] === 7) && xFilled[2] === xFilled[0] + 2 ) || 		 
-			(lastX === xFilled[0] + 6 && xFilled.indexOf(lastX-3) !== -1) ||
-			(lastX === 9 && lastX === xFilled[0] + 8 && xFilled.indexOf(lastX-4) !== -1) ||
-			(lastX === 7 && lastX === xFilled[1] + 4 && xFilled.indexOf(lastX -2) !== -1) 
-			) {
-			$title.text('X is the winner!');
+	var winner = "austin";
+	$('.box').on('click', function() {
+		var $this = $(this);
+		if (count%2 === 0) {
+			$this.append("<div class='text'>X</div>");
+			xFilled.push(parseInt($this.attr('id')));
+			xFilled = xFilled.sort();
+			testWin(xFilled);
+			console.log("xarray:" + xFilled);
+			winner = "O"
+			count++;
+		} else {
+			$this.append("<div class='text'>O</div>");
+			oFilled.push(parseInt($this.attr('id')));
+			oFilled= oFilled.sort();
+			testWin(oFilled);
+				console.log("oarray:" + oFilled);
+			winner = "X"
+			count++;
+		}
+		$this.off()
+	})
+	function testWin(filled){
+		console.log(filled);
+		var last = filled[filled.length-1]
+		if (
+			( (filled[0] === 1 || filled[0] === 4 || filled[0] === 7) && filled[2] === filled[0] + 2 ) ||
+			(last === filled[0] + 6 && filled.indexOf(last-3) !== -1) ||
+			(last === 9 && last === filled[0] + 8 && filled.indexOf(last-4) !== -1) ||
+			(last === 7 && last === filled[1] + 4 && filled.indexOf(last -2) !== -1)
+		) {
+			$title.text(winner + ' is the winner!');
+			$(".box").off()
 			return;
 		}
-		else if ( 
-			( (oFilled[0] === 1 || oFilled[0] === 4 || oFilled[0] === 7) && oFilled[2] === oFilled[0] + 2 ) || 
-			(lastO === oFilled[0] + 6 && oFilled.indexOf(lastO-3) !== -1) ||
-			(lastO === 9 && lastO === oFilled[0] + 8 && oFilled.indexOf(lastO-4) !== -1) ||
-			(lastO === 7 && lastO === oFilled[1] + 4 && oFilled.indexOf(lastO-2) !== -1) 
-			) {
-			$title.text('O is the winner!');
-			return;
-		}
-		})
 	}
 })
