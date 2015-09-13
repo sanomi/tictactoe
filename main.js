d45c9b4fd620755898d1ae69fcf614f488bbcdf8
Sanomi <snrutledge@gmail.com>
2015-09-13 15:43:36 -0700

    changed logic from numbers to sets of wins

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
 'use strict';
+
+var count = 0;
+var xFilled = [];
+var oFilled = [];
+
 $(document).ready(function() {
-	var count = 0;
-	var xFilled = [];
-	var oFilled = [];
 	var $title = $('#title');
 	var winner = "";
 	$('.box').on('click', function() {
 		var $this = $(this);
 		if (count%2 === 0) {
 			$this.addClass('X');
-			xFilled.push($this.attr('class'));
-			testWin(xFilled);
-			console.log("xarray:" + xFilled);
-			winner = "Skull"
+			xFilled.push($this.children("span").attr('class'));
+			winner = "Bones"
+			testWin(xFilled, winner);
 			count++;
 		} else {
 			$this.addClass('O');
-			oFilled.push($this.attr('class'));
-			testWin(oFilled);
-				console.log("oarray:" + oFilled);
-			winner = "Bones"
+			oFilled.push($this.children("span").attr('class'));
+			winner = "Skull";
+			testWin(oFilled, winner);
 			count++;
 		}
 		$this.off()
 	})
-	function testWin(filled){
+})
+
+function testWin(filled, winner){
+	var $title =$('#title');
 		filled = filled.join(' ')
 		console.log(filled);
-		if( /(AcT|AcM|AcB|DoL|DoM|DoR|DiF|DiB)/g.match(filled) ===3 ) {
+		console.log(filled.match(/AcT/g));
+		if( 
+			filled.match(/AcT/g) && filled.match(/AcT/g).length === 3 || 
+			filled.match(/AcM/g) && filled.match(/AcM/g).length === 3 || 
+			filled.match(/AcB/g) && filled.match(/AcB/g).length === 3 || 
+			filled.match(/DoL/g) && filled.match(/DoL/g).length === 3 ||
+			filled.match(/DoM/g) && filled.match(/DoM/g).length === 3 || 
+			filled.match(/DoR/g) && filled.match(/DoR/g).length === 3 ||
+			filled.match(/DiF/g) && filled.match(/DiF/g).length === 3 ||
+			filled.match(/DiB/g) && filled.match(/DiB/g).length === 3
+			) {
 			$title.text(winner + ' for the win!');
 			$(".box").off()
 		} else if ( oFilled.length + xFilled.length === 9 ){
 			$title.text('Ya both loose.');
 			}
+
 	}
-})
